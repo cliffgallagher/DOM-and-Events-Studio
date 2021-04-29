40616140c6221e90a6d4f65734f33d3e47dc9e49
Cliff Gallagher <scliffordgallagher@gmail.com>
2021-04-29 06:37:37 -0500

    takeoff button changes flight status

--- scripts.js
+++ scripts.js
@@ -1,2 +1,22 @@
 // Write your JavaScript code here.
 // Remember to pay attention to page loading!
+function init() {
+    //console.log('window loaded');
+    const takeOffButton = document.getElementById("takeoff");
+    const flightStatus = document.getElementById("flightStatus");
+
+    function takeOffClicked() {
+        const takeOffConfirmation = window.confirm("Confirm that the shuttle is ready for takeoff.");
+
+        if (takeOffConfirmation) {
+            flightStatus.innerHTML = "Shuttle in flight.";
+            
+        }
+
+    }
+
+    takeOffButton.addEventListener("click", takeOffClicked);
+}
+
+
+window.addEventListener("load", init);
