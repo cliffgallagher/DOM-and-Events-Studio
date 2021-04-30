f2c428558592900a6c470e955ab6fb9579e309c6
Cliff Gallagher <scliffordgallagher@gmail.com>
2021-04-30 15:29:55 -0500

    programmed landing and abort buttons

--- scripts.js
+++ scripts.js
@@ -4,18 +4,40 @@ function init() {
     //console.log('window loaded');
     const takeOffButton = document.getElementById("takeoff");
     const flightStatus = document.getElementById("flightStatus");
+    const shuttleBackground = document.getElementById("shuttleBackground");
+    const spaceShuttleHeight = document.getElementById("spaceShuttleHeight");
+    const landingButton = document.getElementById("landing");
+    const missionAbortButton = document.getElementById("missionAbort");
 
     function takeOffClicked() {
         const takeOffConfirmation = window.confirm("Confirm that the shuttle is ready for takeoff.");
 
         if (takeOffConfirmation) {
             flightStatus.innerHTML = "Shuttle in flight.";
-            
+            shuttleBackground.style.backgroundColor = "blue";
+            spaceShuttleHeight.innerHTML = "10,000";
         }
 
     }
 
+    function landingClicked() {
+        window.alert("The shuttle is landing. Landing gear engaged.");
+        flightStatus.innerHTML = "The shuttle has landed.";
+        shuttleBackground.style.backgroundColor = "green";
+        spaceShuttleHeight.innerHTML = "0";
+    }
+
+    function missionAbortClicked() {
+        if (window.confirm("Confirm that you want to abort the mission.")) {
+            flightStatus.innerHTML = "Mission aborted.";
+            shuttleBackground.style.backgroundColor = "green";
+            spaceShuttleHeight.innerHTML = "0";
+        }
+    }
+
     takeOffButton.addEventListener("click", takeOffClicked);
+    landingButton.addEventListener("click", landingClicked);
+    missionAbortButton.addEventListener("click", missionAbortClicked);
 }
 
 
